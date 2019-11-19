a71627a593762817b9361734b456b769705b9bb4
Huku1225 <fksoccer1225@icloud.com>
2019-11-19 18:33:41 +0900

    ObjectManager内の関数仕様変更

--- Libraly/Manager/ObjectManager.h
+++ Libraly/Manager/ObjectManager.h
@@ -3,20 +3,25 @@
 
 #include "../Object/ObjectBase.h"
 
+
 class ObjectManager
 {
 public:
 	ObjectManager();
 	~ObjectManager();
 
+	void Init();
 	void CreateObject();
 	void Update();
 	void Draw();
 
 
 private:
+	static const int MAX_CHARA_OBJ = 50;
+	static const int MAX_BULLET_OBJ = 100;
+
 	ObjectBase* chara_objects[50];
-	ObjectBase* bullets_objects[100];
+	ObjectBase* bullets_objects[MAX_BULLET_OBJ];
 
 };
 
