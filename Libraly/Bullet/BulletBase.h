71c09ad1a8d515a89d380f1b62e3b556e2651ac7
田中健太 <dorakenn1215@gmail.com>
2019-11-30 00:52:43 +0900

    Bulletの描画・アニメーション準備とEffect画像の追加を行いました

--- Libraly/Bullet/BulletBase.h
+++ Libraly/Bullet/BulletBase.h
@@ -11,12 +11,31 @@ public:
 	virtual void Init();
 	virtual void Update();
 	virtual void Load();
-	void Create() {}
+			void Draw();
+			void Create() {}
 
 private:
+	/*アニメーション更新*/
+	void AnimationUpdate();
+	/*動作更新*/
+	void MoveUpdate();
+
+	const float M_BULLET_SYZE = 128.f;
+	
+	/*アニメーション用(仮メンバ)*/
+	int m_anim_timer;
+	
 
 protected:
+	/*アニメーション用(仮メンバ)*/
+	//将来的にはObjectbaseに欲しい
+	int		m_anim_flame;		//切り替えフレーム数
+	int		m_anim_tex_width;	//横分割数
+	int		m_anim_tex_height;	//縦分割数
+	int		m_anim_tex_all;		//分割総数
 
+	float	m_move_count;		//移動量
+	float	m_move_limit;		//移動制限
 
 };
 
