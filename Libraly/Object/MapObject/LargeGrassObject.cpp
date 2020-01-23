e03f56e6227acf8da2843a94cbff176640e160d8
Huku1225 <fksoccer1225@icloud.com>
2020-01-23 14:22:32 +0900

    push Collision

--- Libraly/Object/MapObject/LargeGrassObject.cpp
+++ Libraly/Object/MapObject/LargeGrassObject.cpp
@@ -1,12 +1,17 @@
 #include"LargeGrassObject.h"
 #include"../../Engine/Graphics.h"
 #include"../../Object/Definition.h"
+#include"../../Collision/ShapeType/ShapeCircle.h"
+#include "../../Collision/ShapeType/ShapeRect.h"
+
 
 LargeGrassObject::LargeGrassObject()
 {
 	m_obj_ravel = ObjectRavel::Ravel_MapObj;
 	m_pos.x = 1900.0f;
 	m_pos.y = P_posYforest-100;
+
+	m_shape_list.push_back(new ShapeRect(m_pos.x, 137.0f, m_pos.y, 322.0f, 24.0f, 294.0f, 512.0f));
 
 }
 
